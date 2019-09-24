f1f60ed0e08f79744668d8aabe2e51ae6211b79f
ure8562 <55536830+ure8562@users.noreply.github.com>
2019-09-24 22:45:05 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,86 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2014 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "stm32F4xx.h"
+#include "stm32F4xx_HAL.h"
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F4 empty sample (trace via ITM).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the ITM output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+GPIO_InitTypeDef LED0;
+
+void ms_delay_int_count(volatile unsigned int nTime)
+{
+	nTime = (nTime * 14000);
+	for(; nTime > 0; nTime--);
+}
+
+
+int
+main(int argc, char* argv[])
+{
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+	LED0.Pin = GPIO_PIN_0;
+	LED0.Mode = GPIO_MODE_OUTPUT_PP;
+	LED0.Pull = GPIO_NOPULL;
+	LED0.Speed = GPIO_SPEED_LOW;
+	HAL_GPIO_Init(GPIOC, &LED0);
+	while (1)
+	{
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, 1);
+		ms_delay_int_count(100);
+
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, 0);
+		ms_delay_int_count(100);
+	}
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
